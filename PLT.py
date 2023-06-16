9a8f3624b79db962812ff90c5c9dcfc14abc0446
Abby Baskind (she/they) <85967462+abby-baskind@users.noreply.github.com>
2023-06-16 09:53:10 -0400

    new buoy functions

--- PLT.py
+++ PLT.py
@@ -321,7 +321,7 @@ class PLT():
     
         # # Drop missing data
         for ind in surfaceMV.index:
-            if math.isnan(surfaceMV['surface pH'][ind]) or math.isnan(surfaceMV['surface DO Conc'][ind] or math.isnan(surfaceMV['surface Temp'][ind] or math.isnan(surfaceMV['surface Salinity'][ind]):
+            if math.isnan(surfaceMV['surface pH'][ind]) or math.isnan(surfaceMV['surface DO Conc'][ind]) or math.isnan(surfaceMV['surface Temp'][ind]) or math.isnan(surfaceMV['surface Salinity'][ind]):
                 surfaceMV = surfaceMV.drop(ind)
         surfaceMV = surfaceMV.reset_index(drop=True)
         # # --------------------------------------------------------------------------------------------------------
@@ -335,4 +335,80 @@ class PLT():
                         'surface Salinity', 'surface DO Conc',
                         'pH total']]
         
+        return df
+                                                                                                                    
+    def get_buoy(start_date, end_date, buoy, sensor):
+        """
+        Retrieve data from Andy Davies' sensors using the API
+        Choose from PAR, SUNA, or MetData sensors for, respectively,
+            - light
+            - nutrients
+            - meteorological data
+        Drop 0 or negative pH values
+        Time zone kept in UTC
+        
+        INPUTS:
+            - start_date: date as a string, formatted as YYYY-MM-DD
+            - end_date: date as a string, formatted as YYYY-MM-DD
+            - buoy: Jamestown/620/PLT or 720/Greenwich Bay/BG
+            - PAR, SUNA, or MetData sensors
+        
+        RETURNS pandas DataFrame
+        
+        Add '%cd directory' and '%run PLT.py' to your script, making sure the path is correct
+        """
+        import re
+        import time
+        import PyCO2SYS as pyco2
+        import requests
+        import pandas as pd
+        import numpy as np
+        from datetime import datetime, timedelta
+        import math
+    
+        # Check that start and end dates are formatted correctly
+        # Return error message if format is wrong
+        matched_start = re.match("\d{4}-\d{2}-\d{2}", start_date)
+        matched_end = re.match("\d{4}-\d{2}-\d{2}", end_date)
+        if bool(not matched_start) or bool(not matched_end):
+            print('Error: Input date must be a string of the form YYYY-MM-DD')
+            return
+    
+        # Check that start date is before end date
+        # Switch dates if start date after end date
+        a = time.strptime(start_date, '%Y-%m-%d')
+        b = time.strptime(end_date, '%Y-%m-%d')
+        if a > b:
+            start = start_date
+            start_date = end_date
+            end_date = start
+    
+        # create URL string using start date, end date, and buoy
+        if buoy == 'Jamestown' or buoy == '620' or buoy == 'PLT': 
+            URL = 'https://api.riddc.brown.edu/telemetry/Buoy-620/CoreMetrics/range?start=' + start_date + '&end=' + end_date
+        elif buoy == 'Greenwich Bay' or buoy == '720' or buoy == 'GB': 
+            URL = 'https://api.riddc.brown.edu/telemetry/Buoy-720/CoreMetrics/range?start=' + start_date + '&end=' + end_date
+        else:
+            print('Buoy options are 620 for Jamestown/PLT or 720 for Greenwich Bay/GB.')
+            return                                                                                                          
+    
+        # Ingest data from API
+        r = requests.get(URL)
+        JSON = r.json()
+        df = pd.DataFrame(JSON[sensor])
+    
+        # Drop 0 and negative values and nan values
+        for k in df.keys():
+            for ind in df.index:
+                if isinstance(df[k][ind], float) or isinstance(df[k][ind], int):
+                    if math.isnan(df[k][ind]):
+                        df = df.drop(ind)
+                        df = df.reset_index(drop=True)
+            
+        # Get DateTime object from TmStamp string 
+        # Keep timezone in UTC
+        df['DateTime'] = np.zeros(len(df['TmStamp']))
+        for ind in df.index:
+            df['DateTime'][ind] = datetime.strptime(df['TmStamp'][ind], '%Y-%m-%dT%H:%M:%S.%fZ') 
+    
         return df
